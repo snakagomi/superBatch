2910846bb1a475b35f37aff4de6773279d5fce54
snakagomi <tanakagoro0319@yahoo.ne.jp>
2022-12-29 00:17:11 +0900

    step5に通らない

--- force-app/main/default/lwc/boatSearch/boatSearch.js
+++ force-app/main/default/lwc/boatSearch/boatSearch.js
@@ -1,4 +1,4 @@
- // imports
+// imports
 import { LightningElement } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation';
 export default class BoatSearch extends LightningElement {
--- force-app/main/default/lwc/boatSearchForm/boatSearchForm.js
+++ force-app/main/default/lwc/boatSearchForm/boatSearchForm.js
@@ -1,4 +1,8 @@
 // imports
+
+import {LightningElement, wire, track } from "lwc";
+import getBoatTypes from '@salesforce/apex/BoatDataService.getBoatTypes';
+
 // import getBoatTypes from the BoatDataService => getBoatTypes method';
 export default class BoatSearchForm extends LightningElement {
     selectedBoatTypeId = '';
@@ -7,13 +11,16 @@ export default class BoatSearchForm extends LightningElement {
     error = undefined;
 
     // Needs explicit track due to nested data
+    @track
     searchOptions;
 
     // Wire a custom Apex method
+    @wire(getBoatTypes)
     boatTypes({ error, data }) {
     if (data) {
         this.searchOptions = data.map(type => {
           // TODO: complete the logic
+          return { label: type.Name, value: type.Id };
         });
         this.searchOptions.unshift({ label: 'All Types', value: '' });
     } else if (error) {
@@ -25,9 +32,14 @@ export default class BoatSearchForm extends LightningElement {
     // Fires event that the search option has changed.
     // passes boatTypeId (value of this.selectedBoatTypeId) in the detail
     handleSearchOptionChange(event) {
+      this.selectedBoatTypeId = event.detail.value
       // Create the const searchEvent
       // searchEvent must be the new custom event search
-        searchEvent;
-        this.dispatchEvent(searchEvent);
+      const searchEvent = new CustomEvent('search', {
+        detail: {
+            boatTypeId: this.selectedBoatTypeId
+        }
+      });
+      this.dispatchEvent(searchEvent);
     }
 }
