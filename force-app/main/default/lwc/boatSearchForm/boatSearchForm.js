4ed2ff4e2af2a281174f43c136764f40c9fdbb85
snakagomi <tanakagoro0319@yahoo.ne.jp>
2023-01-10 22:50:51 +0900

    コードをフォーマット

--- force-app/main/default/lwc/boatSearchForm/boatSearchForm.js
+++ force-app/main/default/lwc/boatSearchForm/boatSearchForm.js
@@ -1,44 +1,44 @@
 // imports
 
-import {LightningElement, wire, track } from "lwc";
-import getBoatTypes from '@salesforce/apex/BoatDataService.getBoatTypes';
+import { LightningElement, wire, track } from "lwc";
+import getBoatTypes from "@salesforce/apex/BoatDataService.getBoatTypes";
 
 // import getBoatTypes from the BoatDataService => getBoatTypes method';
 export default class BoatSearchForm extends LightningElement {
-    selectedBoatTypeId = '';
+  selectedBoatTypeId = "";
 
-    // Private
-    error = undefined;
+  // Private
+  error = undefined;
 
-    // Needs explicit track due to nested data
-    @track
-    searchOptions;
+  // Needs explicit track due to nested data
+  @track
+  searchOptions;
 
-    // Wire a custom Apex method
-    @wire(getBoatTypes)
-    boatTypes({ error, data }) {
+  // Wire a custom Apex method
+  @wire(getBoatTypes)
+  boatTypes({ error, data }) {
     if (data) {
-        this.searchOptions = data.map(type => {
-          // TODO: complete the logic
-          return { value: type.Id, label: type.Name};
-        });
-    } else if (error) {
-        this.searchOptions = undefined;
-        this.error = error;
-    }
-    }
-
-    // Fires event that the search option has changed.
-    // passes boatTypeId (value of this.selectedBoatTypeId) in the detail
-    handleSearchOptionChange(event) {
-      this.selectedBoatTypeId = event.detail.value
-      // Create the const searchEvent
-      // searchEvent must be the new custom event search
-      const searchEvent = new CustomEvent('search', {
-        detail: {
-            boatTypeId: this.selectedBoatTypeId
-        }
+      this.searchOptions = data.map((type) => {
+        // TODO: complete the logic
+        return { value: type.Id, label: type.Name };
       });
-      this.dispatchEvent(searchEvent);
+    } else if (error) {
+      this.searchOptions = undefined;
+      this.error = error;
     }
+  }
+
+  // Fires event that the search option has changed.
+  // passes boatTypeId (value of this.selectedBoatTypeId) in the detail
+  handleSearchOptionChange(event) {
+    this.selectedBoatTypeId = event.detail.value;
+    // Create the const searchEvent
+    // searchEvent must be the new custom event search
+    const searchEvent = new CustomEvent("search", {
+      detail: {
+        boatTypeId: this.selectedBoatTypeId
+      }
+    });
+    this.dispatchEvent(searchEvent);
+  }
 }
--- force-app/main/default/lwc/fiveStarRating/fiveStarRating.js
+++ force-app/main/default/lwc/fiveStarRating/fiveStarRating.js
@@ -1,14 +1,14 @@
 //import fivestar static resource, call it fivestar
-import fivestar from '@salesforce/resourceUrl/fivestar';
-import { LightningElement, api } from 'lwc';
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import { loadStyle, loadScript } from 'lightning/platformResourceLoader';
+import fivestar from "@salesforce/resourceUrl/fivestar";
+import { LightningElement, api } from "lwc";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+import { loadStyle, loadScript } from "lightning/platformResourceLoader";
 
 // add constants here
-const ERROR_TITLE = 'Error loading five-star';
-const ERROR_VARIANT = 'error';
-const EDITABLE_CLASS = 'c-rating';
-const READ_ONLY_CLASS = 'readonly c-rating';
+const ERROR_TITLE = "Error loading five-star";
+const ERROR_VARIANT = "error";
+const EDITABLE_CLASS = "c-rating";
+const READ_ONLY_CLASS = "readonly c-rating";
 
 export default class FiveStarRating extends LightningElement {
   //initialize public readOnly and value properties
@@ -39,23 +39,24 @@ export default class FiveStarRating extends LightningElement {
   //display a toast with error message if there is an error loading script
   loadScript() {
     Promise.all([
-      loadScript(this, fivestar + '/rating.js'),
-      loadStyle(this, fivestar + '/rating.css')      
-    ]).then(() => {
-      this.initializeRating();
-    })
-    .catch(error => {
-      const toast = new ShowToastEvent({
+      loadScript(this, fivestar + "/rating.js"),
+      loadStyle(this, fivestar + "/rating.css")
+    ])
+      .then(() => {
+        this.initializeRating();
+      })
+      .catch((error) => {
+        const toast = new ShowToastEvent({
           title: ERROR_TITLE,
           message: error.message,
-          variant: ERROR_VARIANT,
+          variant: ERROR_VARIANT
+        });
+        this.dispatchEvent(toast);
       });
-      this.dispatchEvent(toast);
-    });
   }
 
   initializeRating() {
-    let domEl = this.template.querySelector('ul');
+    let domEl = this.template.querySelector("ul");
     let maxRating = 5;
     let self = this;
     let callback = function (rating) {
@@ -73,12 +74,12 @@ export default class FiveStarRating extends LightningElement {
 
   // Method to fire event called ratingchange with the following parameter:
   // {detail: { rating: CURRENT_RATING }}); when the user selects a rating
-  ratingChanged(rating) {     
-    const ratingchangeEvent = new CustomEvent('ratingchange', {
+  ratingChanged(rating) {
+    const ratingchangeEvent = new CustomEvent("ratingchange", {
       detail: {
         rating: rating
       }
     });
-    this.dispatchEvent(ratingchangeEvent);    
+    this.dispatchEvent(ratingchangeEvent);
   }
 }
